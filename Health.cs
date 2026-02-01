e80b001f8688b9807ddade7794591830495bccd7
meteyilmazbas1 <mete.yilmazbas@sabanciuniv.edu>
2026-02-01 23:27:11 +0200

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,69 @@
+
+using UnityEngine;
+
+namespace UrbanNinja
+{
+    public class Health : MonoBehaviour
+    {
+        private int _maxHealth = 3;
+        private int _currentHealth;
+
+        public int MaxHealth => _maxHealth;
+        public delegate void OnDeath();
+        public event OnDeath OnDeathEvent;
+        public delegate void OnHealthChanged(int currentHealth);
+        public event OnHealthChanged OnHealthChangedEvent;
+
+        /// <summary>
+        /// Set max health here, because it might vary
+        /// between spawns when the asset is reused by
+        /// the object pool.
+        /// </summary>
+        /// <param name="hitPoints">Hitpoints from data.</param>
+        public void SetMaxHealth(int hitPoints)
+        {
+            _maxHealth = hitPoints;
+            ResetHealth();
+        }
+
+        /// <summary>
+        /// Reset health here, because enemies etc.
+        /// are being spawned from an object pool.
+        /// They are not destroyed, but disabled and
+        /// enabled repeatedly.
+        /// </summary>
+        private void OnEnable()
+        {
+            ResetHealth();
+        }
+
+        private void ResetHealth()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            _currentHealth -= amount;
+            //Debug.Log($"{gameObject.name} took {amount} damage. HP now: {_currentHealth}");
+            OnHealthChangedEvent?.Invoke(_currentHealth);
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+        public void TakeHealing(int amount)
+        {
+            _currentHealth += amount;
+            OnHealthChangedEvent?.Invoke(_currentHealth);
+        }
+
+        private void Die()
+        {
+            //Debug.Log($"{gameObject.name} died");
+            OnDeathEvent?.Invoke();
+            gameObject.SetActive( false );
+
+        }
+    }
+}
