1d497364ee416b6d70180b995af65a29010ed31f
Heikki Viljanen <hmvilj@utu.fi>
2026-02-25 20:34:54 +0200

    Modified some tough guy animations and completed it's implementation. Made an extentions class.

---
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -0,0 +1,24 @@
+
+using System;
+using System.Collections.Generic;
+
+public static class Extensions
+{
+    public static int WordCount(this string str) =>
+        str.Length;
+    public static void Shuffle<T>(this List<T> list) 
+    {
+        Random random = new Random();
+        int count = list.Count;
+        
+        for(int i = 0; i < count; i++)
+        {
+            int randomIndex = random.Next(0, count);
+            T current = list[randomIndex];
+            list[randomIndex] = list[i];
+            list[i] = current;
+        }
+    }
+
+}
+
